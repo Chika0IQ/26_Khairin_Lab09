e82ba1cd8920868910a07094e750b06d64f856d7
Chika0IQ <mohdkhairin62@gmail.com>
2020-11-09 22:14:55 +0800

    Comments and Backward

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -12,8 +12,6 @@ public class Character : MonoBehaviour
     public float PositionX;
     public float PositionY;
     public float PositionZ;
-
-
     public GameObject Text;
     
     // Start is called before the first frame update
@@ -31,23 +29,32 @@ public class Character : MonoBehaviour
         PositionZ = gameObject.transform.position.z;
 
         
-
+        // Forward Movement
         if (Input.GetKey(KeyCode.W))
         {
             //transform.position += new Vector3(0,0, Time.deltaTime * speed);
             transform.position += transform.forward * speed * Time.deltaTime;
-
         }
+
+        // Backward Movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        // Rotate right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0, Time.deltaTime * rotatespeed, 0));
         }
 
+        //Rotate Left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, Time.deltaTime * -rotatespeed, 0));
         }
 
+        //Display Position Cords
         Text.GetComponent<Text>().text = "Postion: ( " + PositionX + ", " + PositionY + ", " + PositionZ + " )";
         
 
