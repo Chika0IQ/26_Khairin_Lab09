83d5f7af6879affc7396932e31d5319b0c94abb7
Chika0IQ <mohdkhairin62@gmail.com>
2020-11-09 11:58:04 +0800

    Initial commit

---
+++ Assets/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+
+    public float speed;
+    public float rotatespeed;
+
+    public float PositionX;
+    public float PositionY;
+    public float PositionZ;
+
+
+    public GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionX = gameObject.transform.position.x;
+        PositionY = gameObject.transform.position.y;
+        PositionZ = gameObject.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PositionX = gameObject.transform.position.x;
+        PositionY = gameObject.transform.position.y;
+        PositionZ = gameObject.transform.position.z;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0,0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotatespeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotatespeed, 0));
+        }
+
+        Text.GetComponent<Text>().text = "Postion: (" + PositionX + "," + PositionY + "," + PositionZ + ")";
+
+    }
+}
